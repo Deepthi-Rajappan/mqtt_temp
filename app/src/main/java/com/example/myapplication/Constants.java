6c7c39be856778a9d013d93446664f93b5d3aa0f
Deepthi-Rajappan <55912155+Deepthi-Rajappan@users.noreply.github.com>
2019-11-23 18:36:28 +0000

    everything

---
+++ app/src/main/java/com/example/myapplication/Constants.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://10.6.42.165:1883";
+
+    public static final String PUBLISH_TOPIC = "#";
+
+    public static final String CLIENT_ID = "androidkt";
+}
